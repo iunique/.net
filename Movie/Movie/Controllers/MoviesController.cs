130bdf8b2e4607e3d8f966413a740a8a554652c5
iunique <845723932@qq.com>
2018-10-28 10:24:00 +0800

    D:/Git/Movies/create实现插入，/Movies/index为主目录

---
+++ Movie/Movie/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using Movie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();        public ActionResult Index()        {
+            return View(db.Movies.ToList());        }
+
+        public ActionResult Create()        {            return View();        }
+        [HttpPost]        public ActionResult Create(Movies movie)        {
+            db.Movies.Add(movie);            db.SaveChanges();            return RedirectToAction("Index");        }        public ActionResult Delete(int id)        {
+            Movies movies=db.Movies.Find(id);            db.Movies.Remove(movies);            db.SaveChanges();            return RedirectToAction("Index");        }        public ActionResult Edit(int id)        {            Movies movies = db.Movies.Find(id);            return View(movies);        }        [HttpPost]        public ActionResult Adj(Movies movie)        {
+            var result = from m in db.Movies
+                         where m.ID==movie.ID
+                         select m;            foreach (Movies m in result)
+            {
+                m.Price = movie.Price;
+                m.ReleaseDate = movie.ReleaseDate;
+                m.Title = movie.Title;
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");        }
+    }
+}
---
+++ Movie/Movie/Models/MovieDBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Movie.Models
+{
+    public class MovieDBContext : DbContext
+    {        public DbSet<Movies> Movies { get; set; }
+    }
+}
